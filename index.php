9371f12ea48f2b967d7e59ec7f50291ac03fa7fb
sbudumyan <s.budumyan@gmail.com>
2017-06-23 02:26:39 +0300

    Init index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+define("ROOTDIR", __DIR__);
+
+require __DIR__.'/vendor/autoload.php';
+
+
+
+
+//print_r( Core\Config::get("commands") );
+//print_r( Twpf\Command\CommandManager::executeCommand("tesst") );
+//echo json_encode( \Twpf\Command\CommandManager::getList()) ;
